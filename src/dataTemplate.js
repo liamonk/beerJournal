1e2e9a87bf1783ec916eb9a4406730354f74d8de
liam <liamokeegan@gmail.com>
2023-06-25 11:21:02 +1200

    adding a template for new data entries

---
+++ src/dataTemplate.js
@@ -0,0 +1,18 @@
+/*
+
+Template to use for adding new posts
+
+{
+    id: ,
+    name: "",
+    style: "",
+    styleGuideAddress: "",
+    brewDate: "",
+    reviewDate: "",
+    description: "",
+    review:"",
+    image:"",
+    rating:""
+},
+
+*/
