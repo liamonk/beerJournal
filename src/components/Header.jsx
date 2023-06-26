0809102eaebd5e1bd88774bea8c190007135f5eb
liam <liamokeegan@gmail.com>
2023-06-26 21:02:12 +1200

    css change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <header className="heading">
-      <img className="heading-icon" src="./hops.png"></img>
+      <div style={{display: "flex", alignItems: "center", justifyContent:"center"}}><img className="heading-icon" src="./hops.png"></img></div>
       <h1>my brewing diary</h1>
     </header>
   );
